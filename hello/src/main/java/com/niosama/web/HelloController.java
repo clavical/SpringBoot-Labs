f0aa220b67fea431e2e09e57d32e0c9f3dadf4e8
yezhiwei <453183259@qq.com>
2020-12-10 18:48:09 +0800

    <ribbon>配置代码提交1

--- hello/src/main/java/com/niosama/web/HelloController.java
+++ hello/src/main/java/com/niosama/web/HelloController.java
@@ -1,32 +1,20 @@
 package com.niosama.web;
 
-import com.netflix.appinfo.InstanceInfo;
-import com.netflix.discovery.DiscoveryClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.serviceregistry.Registration;
-import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 public class HelloController {
     private static final Logger log = LoggerFactory.getLogger(HelloController.class);
-    @Autowired
-    private Registration registration; // 服务注册
-    @Autowired
-    private DiscoveryClient client;
 
-    @RequestMapping
+    @RequestMapping(value = "/hello")
     public String index(){
-        List<InstanceInfo> instances = client.getInstancesById(registration.getServiceId());
-        if(!CollectionUtils.isEmpty(instances)){
-        }
-        InstanceInfo.InstanceStatus status = client.getInstanceRemoteStatus();
+//        List<InstanceInfo> instances = client.getInstancesById(registration.getServiceId());
+//        if(!CollectionUtils.isEmpty(instances)){
+//        }
+//        InstanceInfo.InstanceStatus status = client.getInstanceRemoteStatus();
         return "Hello World";
     }
 
---
+++ ribbon-consumer/src/test/java/com/niosama/ribbonconsumer/RibbonConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niosama.ribbonconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RibbonConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
